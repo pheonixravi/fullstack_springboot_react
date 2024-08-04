b799e11c82c1305f181e2392049c2706d0b66f84
scott <scott@gmail.com>
2024-08-04 15:49:16 +0530

    initial commit

---
+++ src/main/java/in/ravi/RestapiBackendApplication.java
@@ -0,0 +1,13 @@
+package in.ravi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestapiBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestapiBackendApplication.class, args);
+	}
+
+}
